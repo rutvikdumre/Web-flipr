e40c0b96e3a6d488e9dee1a73abf5d7bfdeecf5f
rutvikdumre <45359655+rutvikdumre@users.noreply.github.com>
2020-08-14 20:49:03 +0530

    mainpage

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('mainpage.html')
 
 if __name__ == '__main__':
     app.run()
